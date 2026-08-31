cf5b0a1285c90b20819d12217bbf1d7876ddef21
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-08-30 21:04:31 -0400

    Reorganize into directories. Create dummy localhost resources

--- upload.sh
+++ upload.sh
@@ -1,31 +1,59 @@
 #!/usr/bin/env bash
 
-curl --fail-with-body \
-    -X POST http://127.0.0.1:8080/api/v1alpha1/servers \
-    -H 'Content-Type: application/yaml' \
-    --data-binary @desktop.yaml
 
-curl --fail-with-body \
-    -X POST http://127.0.0.1:8080/api/v1alpha1/secret-stores \
-    -H 'Content-Type: application/yaml' \
-    --data-binary @openbao-secret-store.yaml
+# Upload all GitRepository resources
+for file in GitRepository/*.yaml; do
+    echo "Uploading GitRepository resource from $file"
+    curl --fail-with-body \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file" \
+        http://127.0.0.1:8080/api/v1alpha1/git-repositories
+done
 
-curl --fail-with-body \
-    -X POST http://127.0.0.1:8080/api/v1alpha1/ssh-access-grants \
-    -H 'Content-Type: application/yaml' \
-    --data-binary @desktop-matt-ssh.yaml
+for file in InventoryCaptureGroup/*.yaml; do
+    echo "Uploading InventoryCaptureGroup resource from $file"
+    curl --fail-with-body \
+        -X POST http://127.0.0.1:8080/api/v1alpha1/inventory-capture-groups \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file"
+done
 
-curl --fail-with-body \
-    -X POST http://127.0.0.1:8080/api/v1alpha1/inventory-capture-groups \
-    -H 'Content-Type: application/yaml' \
-    --data-binary @server-capture-group.yaml
+for file in InventoryPublication/*.yaml; do
+    echo "Uploading InventoryPublication resource from $file"
+    curl --fail-with-body \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file" \
+        http://127.0.0.1:8080/api/v1alpha1/inventory-publications
+done
 
-curl --fail-with-body \
-    -H 'Content-Type: application/yaml' \
-    --data-binary @git-repos.yaml \
-    http://127.0.0.1:8080/api/v1alpha1/git-repositories
+for file in SecretStore/*.yaml; do
+    echo "Uploading SecretStore resource from $file"
+    curl --fail-with-body \
+        -X POST http://127.0.0.1:8080/api/v1alpha1/secret-stores \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file"
+done
 
-curl --fail-with-body \
-    -H 'Content-Type: application/yaml' \
-    --data-binary @server-inventory-publication.yaml \
-    http://127.0.0.1:8080/api/v1alpha1/inventory-publications
+for file in MachineReport/*.yaml; do
+    echo "Uploading MachineReport resource from $file"
+    curl --fail-with-body \
+        -X POST http://127.0.0.1:8080/api/v1alpha1/machine-reports \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file"
+done
+
+for file in Server/*.yaml; do
+    echo "Uploading Server resource from $file"
+    curl --fail-with-body \
+        -X POST http://127.0.0.1:8080/api/v1alpha1/servers \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file"
+done
+
+for file in SSHAccessGrant/*.yaml; do
+    echo "Uploading SSHAccessGrant resource from $file"
+    curl --fail-with-body \
+        -X POST http://127.0.0.1:8080/api/v1alpha1/ssh-access-grants \
+        -H 'Content-Type: application/yaml' \
+        --data-binary @"$file"
+done
